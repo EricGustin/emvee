d19d3f642ba28966d620b5cbfb6b33c99ecdfc0b
Gustiin <ericgustin44@gmail.com>
2020-06-29 18:53:11 -0700

    Migrated sign up VC to programmatic

--- emvee/View Controllers/SignUpViewController.swift
+++ emvee/View Controllers/SignUpViewController.swift
@@ -14,14 +14,22 @@ class SignUpViewController: UIViewController {
   
   private var datePicker: UIDatePicker?
   
-  
-  @IBOutlet weak var firstNameTextField: UITextField!
-  @IBOutlet weak var lastNameTextField: UITextField!
-  @IBOutlet weak var emailTextField: UITextField!
-  @IBOutlet weak var passwordTextField: UITextField!
-  @IBOutlet weak var dateOfBirthTextField: UITextField!
-  @IBOutlet weak var signUpButton: UIButton!
-  @IBOutlet weak var errorLabel: UILabel!
+  private var section1VerticalStack: UIStackView?
+  private var firstNameTextField: UITextField?
+  private var lastNameTextField: UITextField?
+  private var emailTextField: UITextField?
+  private var passwordTextField: UITextField?
+  private var dateOfBirthTextField: UITextField?
+  private var section2VerticalStack: UIStackView?
+  private var signUpButton: UIButton?
+  private var errorLabel: UILabel?
+//  @IBOutlet weak var firstNameTextField: UITextField!
+//  @IBOutlet weak var lastNameTextField: UITextField!
+//  @IBOutlet weak var emailTextField: UITextField!
+//  @IBOutlet weak var passwordTextField: UITextField!
+//  @IBOutlet weak var dateOfBirthTextField: UITextField!
+//  @IBOutlet weak var signUpButton: UIButton!
+//  @IBOutlet weak var errorLabel: UILabel!
   //@IBOutlet weak var datePicker: UIDatePicker!
   @IBAction func cancelButtonClicked(_ sender: UIButton) {
     let welcomeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.welcomeViewController) as? WelcomeViewController
@@ -33,45 +41,129 @@ class SignUpViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    print("In signUpViewController")
+
     setUpSubviews()
-    
-    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
-    view.addGestureRecognizer(tapGesture)
-    
-    firstNameTextField.delegate = self
-    lastNameTextField.delegate = self
-    emailTextField.delegate = self
-    passwordTextField.delegate = self
+    setUpGestures()
+    setDelegates()
   }
   
-  func setUpSubviews() {
+  private func setUpSubviews() {
     
+    firstNameTextField = UITextField()
+    firstNameTextField?.textColor = .black
+    firstNameTextField?.placeholder = "First Name"
+    firstNameTextField?.font = UIFont(name: "American Typewriter", size: 16)
+    StyleUtilities.styleTextField(firstNameTextField!, firstNameTextField!.placeholder ?? "")
+    firstNameTextField?.translatesAutoresizingMaskIntoConstraints = false
+    firstNameTextField?.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    
+    lastNameTextField = UITextField()
+    lastNameTextField?.textColor = .black
+    lastNameTextField?.placeholder = "Last Name"
+    lastNameTextField?.font = UIFont(name: "American Typewriter", size: 16)
+    StyleUtilities.styleTextField(lastNameTextField!, lastNameTextField!.placeholder ?? "")
+    lastNameTextField?.translatesAutoresizingMaskIntoConstraints = false
+    lastNameTextField?.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    
+    emailTextField = UITextField()
+    emailTextField?.textColor = .black
+    emailTextField?.placeholder = "Email"
+    emailTextField?.font = UIFont(name: "American Typewriter", size: 16)
+    StyleUtilities.styleTextField(emailTextField!, emailTextField!.placeholder ?? "")
+    emailTextField?.translatesAutoresizingMaskIntoConstraints = false
+    emailTextField?.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    
+    passwordTextField = UITextField()
+    passwordTextField?.textColor = .black
+    passwordTextField?.placeholder = "Password"
+    passwordTextField?.font = UIFont(name: "American Typewriter", size: 16)
+    StyleUtilities.styleTextField(passwordTextField!, passwordTextField!.placeholder ?? "")
+    passwordTextField?.translatesAutoresizingMaskIntoConstraints = false
+    passwordTextField?.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    
+    section1VerticalStack = UIStackView()
+    section1VerticalStack?.axis = .vertical
+    section1VerticalStack?.spacing = 20
+    section1VerticalStack?.translatesAutoresizingMaskIntoConstraints = false
+    section1VerticalStack?.addArrangedSubview(firstNameTextField!)
+    section1VerticalStack?.addArrangedSubview(lastNameTextField!)
+    section1VerticalStack?.addArrangedSubview(emailTextField!)
+    section1VerticalStack?.addArrangedSubview(passwordTextField!)
+    view.addSubview(section1VerticalStack!)
+    section1VerticalStack?.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+    section1VerticalStack?.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
+    section1VerticalStack?.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40).isActive = true
+    section1VerticalStack?.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40).isActive = true
+    
+    dateOfBirthTextField = UITextField()
+    dateOfBirthTextField?.textColor = .black
+    dateOfBirthTextField?.placeholder = "Date Of Birth"
+    dateOfBirthTextField?.font = UIFont(name: "American Typewriter", size: 16)
     datePicker = UIDatePicker()
     datePicker?.datePickerMode = .date
     datePicker?.addTarget(self, action: #selector(dateChanged), for: .valueChanged)
+    dateOfBirthTextField?.inputView = datePicker
+    StyleUtilities.styleTextField(dateOfBirthTextField!, dateOfBirthTextField!.placeholder ?? "")
+    dateOfBirthTextField?.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(dateOfBirthTextField!)
+    dateOfBirthTextField?.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    dateOfBirthTextField?.topAnchor.constraint(equalTo: section1VerticalStack!.bottomAnchor, constant: 40).isActive = true
+    dateOfBirthTextField?.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
+    dateOfBirthTextField?.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40).isActive = true
     
-    dateOfBirthTextField.inputView = datePicker
+    signUpButton = UIButton()
+    signUpButton?.setTitleColor(.white, for: .normal)
+    signUpButton?.setTitle("Sign Up", for: .normal)
+    signUpButton?.titleLabel?.font = UIFont(name: "American Typewriter", size: 16)
+    signUpButton?.addTarget(self, action: #selector(signUpClicked), for: .touchUpInside)
+    StyleUtilities.styleFilledButton(signUpButton!)
+    signUpButton?.translatesAutoresizingMaskIntoConstraints = false
+    signUpButton?.heightAnchor.constraint(equalToConstant: 50).isActive = true
     
-    errorLabel.alpha = 0
-    StyleUtilities.styleTextField(firstNameTextField, firstNameTextField.placeholder ?? "")
-    StyleUtilities.styleTextField(lastNameTextField, lastNameTextField.placeholder ?? "")
-    StyleUtilities.styleTextField(emailTextField, emailTextField.placeholder ?? "")
-    StyleUtilities.styleTextField(passwordTextField, passwordTextField.placeholder ?? "")
-    StyleUtilities.styleTextField(dateOfBirthTextField, dateOfBirthTextField.placeholder ?? "")
-    StyleUtilities.styleFilledButton(signUpButton)
+    errorLabel = UILabel()
+    errorLabel?.textAlignment = .center
+    errorLabel?.numberOfLines = 0
+    errorLabel?.textColor = .systemRed
+    errorLabel?.font = UIFont(name: "American Typewriter", size: 16)
+    errorLabel?.alpha = 0
+    errorLabel?.translatesAutoresizingMaskIntoConstraints = false
+    errorLabel?.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    
+    section2VerticalStack = UIStackView()
+    section2VerticalStack?.axis = .vertical
+    section2VerticalStack?.spacing = 20
+    section2VerticalStack?.translatesAutoresizingMaskIntoConstraints = false
+    section2VerticalStack?.addArrangedSubview(signUpButton!)
+    section2VerticalStack?.addArrangedSubview(errorLabel!)
+    view.addSubview(section2VerticalStack!)
+    section2VerticalStack?.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+    section2VerticalStack?.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
+    section2VerticalStack?.topAnchor.constraint(equalTo: dateOfBirthTextField!.bottomAnchor, constant: 40).isActive = true
+    section2VerticalStack?.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -40).isActive = true
+  }
+  
+  private func setUpGestures() {
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+    view.addGestureRecognizer(tapGesture)
+  }
+  
+  private func setDelegates() {
+    firstNameTextField?.delegate = self
+    lastNameTextField?.delegate = self
+    emailTextField?.delegate = self
+    passwordTextField?.delegate = self
   }
   
   func validateFields() -> String? {
     // Check that all fields are filled in
-    if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
-      lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
-      emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
-      passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
-      dateOfBirthTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+    if firstNameTextField?.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+      lastNameTextField?.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+      emailTextField?.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+      passwordTextField?.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+      dateOfBirthTextField?.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
       return "Please fill in all fields"
     }
-    let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+    let cleanedPassword = passwordTextField!.text!.trimmingCharacters(in: .whitespacesAndNewlines)
     if !(FormUtilities.isPasswordValid(cleanedPassword)) {
       return "Please make sure your password is at least 8 characters, contains a special character and a number."
     }
@@ -79,18 +171,18 @@ class SignUpViewController: UIViewController {
   }
   
   func showError(_ message: String) {
-    errorLabel.text = message
-    errorLabel.alpha = 1
+    errorLabel?.text = message
+    errorLabel?.alpha = 1
   }
   
-  func transitionToHome() {
+  private func transitionToHome() {
     let homeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
     
     view.window?.rootViewController = homeViewController
     view.window?.makeKeyAndVisible()
   }
   
-  @IBAction func signUpClicked(_ sender: Any) {
+  @objc func signUpClicked(_ sender: Any) {
     // Validate fields
     let error = validateFields()
     if error != nil {
@@ -99,11 +191,11 @@ class SignUpViewController: UIViewController {
       // Create user
     else {
       // Create cleaned version of data
-      let firstName = firstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-      let lastName = lastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-      let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-      let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-      let dateOfBirth = dateOfBirthTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+      let firstName = firstNameTextField!.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+      let lastName = lastNameTextField!.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+      let email = emailTextField!.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+      let password = passwordTextField!.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+      let dateOfBirth = dateOfBirthTextField?.text!.trimmingCharacters(in: .whitespacesAndNewlines)
       Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
         // Check for errors
         if err != nil {
@@ -119,7 +211,7 @@ class SignUpViewController: UIViewController {
           db.collection("users").document(userID!).setData([
             "firstName": firstName,
             "lastName": lastName,
-            "birthday": dateOfBirth,
+            "birthday": dateOfBirth!,
             "uid": userID!,
             "bio": "",
           ])
@@ -138,7 +230,7 @@ class SignUpViewController: UIViewController {
   @objc private func dateChanged() {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "MMMM dd yyyy"
-    dateOfBirthTextField.text = dateFormatter.string(from: datePicker!.date)
+    dateOfBirthTextField?.text = dateFormatter.string(from: datePicker!.date)
     view.endEditing(true)
   }
   
